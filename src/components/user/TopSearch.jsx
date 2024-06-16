c6fe48cce4fcc353b67ca5e59ca3cb8e926a1683
JeongWoongGyo <wg.jeong@springworks.co.kr>
2024-06-16 23:13:49 +0900

    feat: 실시간 검색어 순위 기능 연동

--- src/components/user/TopSearch.jsx
+++ src/components/user/TopSearch.jsx
@@ -1,87 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { api } from '../../hooks/useAxiosInterceptor';
 import { GoTriangleDown, GoTriangleUp } from 'react-icons/go';
 import { BsDash } from 'react-icons/bs';
 
 const TopSearch = () => {
+    const [topSearches, setTopSearches] = useState([]);
+    const [searchTime, setSearchTime] = useState('');
+
+    useEffect(() => {
+        (async () => {
+            await fetchTopSearch();
+        })();
+    }, []);
+
+    /**
+     * 실시간 검색어 Top 10 조회
+     */
+    const fetchTopSearch = async () => {
+        try {
+            const res = await api('/api/ranking/recent-top-10-search-keyword');
+            setTopSearches(res.data.data.searchRankResDtos);
+
+            const searchTimeData = res.data.data.searchTime;
+            setSearchTime(searchTimeData.substring(3, searchTimeData.length - 3));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    /**
+     * 랭킹 상태에 따른 아이콘 반환
+     */
+    const getRankState = (rankState) => {
+        if (rankState === 'UP') {
+            return <GoTriangleUp className="text-red-600 w-8 h-8 mr-3" />;
+        } else if (rankState === 'DOWN') {
+            return <GoTriangleDown className="text-blue-600 w-8 h-8 mr-3" />;
+        } else if (rankState === 'NEW') {
+            return <span className="mr-3 font-bold">New</span>;
+        } else {
+            return <BsDash className="w-8 h-8 mr-3" />;
+        }
+    };
     return (
         <div className="bg-white p-4 flex-1 md:w-1/2 md:ml-2">
             <div className="flex justify-between mb-2">
                 <h2 className="text-xl font-bold">가장 많이 검색하고 있어요</h2>
-                <p className="self-end ml-2 text-gray-400 text-sm">06.08 23:00 기준</p>
+                <p className="self-end ml-2 text-gray-400 text-sm">{searchTime} 기준</p>
             </div>
             <ol
                 className="list-inside bg-gray-100 flex flex-col justify-around pl-3"
                 style={{ height: 'calc(500px + 2rem)' }}>
-                <li className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <span className="text-2xl mr-2 font-bold w-4">1</span>
-                        <p>검색어 1</p>
+                {topSearches.length === 0 ? (
+                    <div className="flex justify-center items-center h-full">
+                        <div className="text-3xl font-bold">검색어가 없습니다.</div>
                     </div>
-                    <GoTriangleUp className="text-red-600 w-8 h-8 mr-3" />
-                </li>
-                <li className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <span className="text-2xl mr-2 font-bold w-4">2</span>
-                        <p>검색어 2</p>
-                    </div>
-                    <GoTriangleDown className="text-blue-600 w-8 h-8 mr-3" />
-                </li>
-                <li className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <span className="text-2xl mr-2 font-bold w-4">3</span>
-                        <p>검색어 3</p>
-                    </div>
-                    <GoTriangleUp className="text-red-600 w-8 h-8 mr-3" />
-                </li>
-                <li className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <span className="mr-2 font-bold w-4">4</span>
-                        <p>검색어 4</p>
-                    </div>
-                    <GoTriangleUp className="text-red-600 w-8 h-8 mr-3" />
-                </li>
-                <li className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <span className="mr-2 font-bold w-4">5</span>
-                        <p>검색어 5</p>
-                    </div>
-                    <GoTriangleDown className="text-blue-600 w-8 h-8 mr-3" />
-                </li>
-                <li className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <span className="mr-2 font-bold w-4">6</span>
-                        <p>검색어 6</p>
-                    </div>
-                    <span className="mr-3 font-bold">New</span>
-                </li>
-                <li className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <span className="mr-2 font-bold w-4">7</span>
-                        <p>검색어 7</p>
-                    </div>
-                    <GoTriangleUp className="text-red-600 w-8 h-8 mr-3" />
-                </li>
-                <li className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <span className="mr-2 font-bold w-4">8</span>
-                        <p>검색어 8</p>
-                    </div>
-                    <BsDash className="w-8 h-8 mr-3" />
-                </li>
-                <li className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <span className="mr-2 font-bold w-4">9</span>
-                        <p>검색어 9</p>
-                    </div>
-                    <GoTriangleDown className="text-blue-600 w-8 h-8 mr-3" />
-                </li>
-                <li className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <span className="mr-2 font-bold w-4">10</span>
-                        <p>검색어 10</p>
-                    </div>
-                    <GoTriangleUp className="text-red-600 w-8 h-8 mr-3" />
-                </li>
+                ) : (
+                    topSearches.map((search, index) => (
+                        <li key={search.keyword} className="flex items-center justify-between">
+                            <div className="flex items-center">
+                                <span className={`${index < 3 && 'text-2xl'} mr-2 font-bold w-4`}>
+                                    {index + 1}
+                                </span>
+                                <p>{search.keyword}</p>
+                            </div>
+                            {getRankState(search.rankState)}
+                        </li>
+                    ))
+                )}
             </ol>
         </div>
     );
